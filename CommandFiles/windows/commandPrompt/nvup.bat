fe088f7c2b3ab42b219f93c3e1ef2421859840a7
Suzuki <milmil_13@yahoo.co.jp>
2023-01-05 17:49:48 +0900

    fix nvup.bat

--- CommandFiles/windows/commandPrompt/nvup.bat
+++ CommandFiles/windows/commandPrompt/nvup.bat
@@ -2,6 +2,7 @@
 
 @REM sはサブディレクトリも含めるという意味 qはそのときに確認メッセージなしにする
 rmdir /s /q %HOMEPATH%\GitHub\myEnvironment\nvda
-hは隠しファイルや設定ファイルも含めるという意味　iはコピー先ディレクトリがない場合に新しく作るという意味
-xcopy /H /I /s C:\Users\suzuki\AppData\Roaming\nvda C:\Users\suzuki\GitHub\myEnvironment\nvda
-cd C:\Users\suzuki\GitHub\myEnvironment
+@REM hは隠しファイルや設定ファイルも含めるという意味　iはコピー先ディレクトリがない場合に新しく作るという意味
+xcopy /H /I /s %HOMEPATH%\AppData\Roaming\nvda %HOMEPATH%\GitHub\myEnvironment\nvda
+xcopy /y %HOMEPATH%\Documents\my_scripts\* %HOMEPATH%\GitHub\myEnvironment\commandFiles\windows\commandPrompt
+cd %HOMEPATH%\GitHub\myEnvironment
